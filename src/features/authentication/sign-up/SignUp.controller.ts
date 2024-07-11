5f1f0577f78eb672f093f65d3531668488b8ae01
anji-Derry <anjireddy12382@gmail.com>
2024-07-11 22:00:44 +0530

    Sign-up module updated

--- src/features/authentication/sign-up/SignUp.controller.ts
+++ src/features/authentication/sign-up/SignUp.controller.ts
@@ -1,6 +1,23 @@
 import { Response200, SendErrorResponse } from '../../../utils/Response';
 import { SignUpService } from './SignUp.service';
 import { Request, Response } from 'express';
+import * as yup from 'yup';
+
+const memberSchema = yup.object({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  phone: yup.number().required(),
+  userName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(4).required(),
+  address: yup.object({
+    addressLine: yup.string().required(),
+    city: yup.string().required(),
+    state: yup.string().required(),
+    country: yup.string().required(),
+    zipCode: yup.number().required()
+  })
+});
 
 export class SignUpController {
   private signUpService: SignUpService;
@@ -11,9 +28,13 @@ export class SignUpController {
 
   async createMember(req: Request, res: Response) {
     try {
+      await memberSchema.validate(req.body);
       const result = await this.signUpService.createMember(req.body);
       return res.status(Response200.code).json(result);
     } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return res.status(400).json({ message: error.message });
+      }
       return SendErrorResponse(error, res);
     }
   }
--- src/features/authentication/sign-up/SignUp.service.ts
+++ src/features/authentication/sign-up/SignUp.service.ts
@@ -1,15 +1,18 @@
+import { MailService } from '../../../features/mail-service/MailService';
 import { Member } from '../../../entities/Member';
 import { HttpError, Response404, ResponseConflict } from '../../../utils/Response';
 import { SignUpDao } from './SignUp.dao';
 
 export class SignUpService {
   private signUpDao: SignUpDao;
+  private mailService: MailService;
 
   constructor() {
     this.signUpDao = new SignUpDao();
+    this.mailService = new MailService();
   }
 
-  async createMember(member: Partial<Member>): Promise<Member> {
+  async createMember(member: Member): Promise<Member> {
     if (await this.signUpDao.userExists(member)) {
       throw new HttpError(ResponseConflict.message, ResponseConflict.code);
     }
@@ -17,7 +20,18 @@ export class SignUpService {
     if (!member.password) {
       throw new HttpError('Please provide password', 400);
     }
-    return await this.signUpDao.saveMemberInDB(member);
+    await this.signUpDao.saveMemberInDB(member);
+
+    await this.mailService.send({
+      to: member.email,
+      subject: 'Registration Successful',
+      templateName: 'welcome',
+      replacements: {
+        firstName: member.firstName,
+        lastName: member.lastName
+      }
+    })
+    return member;
   }
 
   async updateMember(member: Member) {
